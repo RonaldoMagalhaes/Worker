171bd8134e1fbd5a0401f4efa0b782d96f503785
RonaldoMagalhaes <ronaldo.magalhaes1@gmail.com>
2023-01-14 22:37:42 -0300

    Final version

--- WorkContract/WorkContract/Entities/Worker.cs
+++ WorkContract/WorkContract/Entities/Worker.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WorkContract.Entities.Enum;
 
 namespace WorkContract.Entities
 {
     internal class Worker
     {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+
+        public Department Department { get; set; }
+
+        public List<HourContract> contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, Department department, double baseSalary)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+        }
+
+        public void addContract(HourContract contract)
+        {
+            contracts.Add(contract);
+        }
+
+        public void removeContract(HourContract contract) 
+        {
+            contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+
+            foreach (HourContract contract in contracts)
+            {
+                if(contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+
+            return sum;
+            
+        }
     }
+
+
 }
--- WorkContract/WorkContract/Program.cs
+++ WorkContract/WorkContract/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using WorkContract.Entities;
+using WorkContract.Entities.Enum;
 
 namespace WorkContract
 {
@@ -10,6 +9,54 @@ namespace WorkContract
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter department´s name: ");
+            string depName = Console.ReadLine();
+            Console.WriteLine("Enter worker data");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level (Junior/MidLevel/Senior):");
+            WorkerLevel level = (WorkerLevel)Enum.Parse(typeof(WorkerLevel), Console.ReadLine());
+            Console.Write("Base Salary: ");
+            double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Department department= new Department(depName);
+            Worker worker = new Worker(name, level, department, salary);
+
+            Console.WriteLine();
+
+            Console.Write("How many contract´s to this worker: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Enter #{i} contract data: ");
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value Per Hour: ");
+                double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Duration: ");
+                int time = int.Parse(Console.ReadLine());
+                HourContract hc = new HourContract(date, value, time);
+                worker.addContract(hc);
+
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Enter month and year to calculate income(MM/YYYY): ");
+            string montAndYear = Console.ReadLine();
+
+            int year = int.Parse(montAndYear.Substring(3,4));
+            int month = int.Parse(montAndYear.Substring(0,2));
+
+                
+            Console.WriteLine();
+            Console.WriteLine("===========================================");
+            Console.WriteLine("Name " + worker.Name);
+            Console.WriteLine("Department " + department.Name.ToString());
+            Console.WriteLine("Income for  " + montAndYear + ": " + worker.Income(year, month));
+
+            Console.ReadLine();
         }
     }
 }
